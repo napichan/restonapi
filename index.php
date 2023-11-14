498948d08ac07d601e6eaab20814ca8a5f99d9de
napichan <dwikiilman01@gmail.com>
2023-11-14 13:56:38 +0700

    restonapi

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Aplikasi Resto Enak</title>
+        <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <h1>Resto Enak</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Hot Promo</a></li>
+                <li><a href="#">Makanan</a></li>
+                <li><a href="#">Minuman</a></li>
+                <li><a href="#">Desert</a></li>
+</ul>
+</nav>
+</header>
+
+<section class="menu">
+    <h2>Menu Resto Enak</h2>
+    <div class="menu-item">
+        <img src="img/Bakso.jpg" alt="menu 1">
+        <h3>Bakso Granat</h3>
+        <p>
+            Deskripsi tentang makanan misalnya nilai gizi dsb.
+</p>
+<p class="harga">Harga : Rp. 10.000</p>
+<button class="order-button">Pesan</button>
+</div>
+<div class="menu-item">
+    <img src="img/Bakso.jpg" alt="menu 2">
+    <h3>Bakso Granat</h3>
+    <p>
+        Deskripsi tentang makanan misalnya nilai gizi dsb.
+</p>
+<p class="harga">Harga : Rp. 10.000</p>
+<button class="order-button">Pesan</button>
+</div>
+</section>
+
+<section class="contact">
+    <h2>Hubungi Kami</h2>
+    <p>Jika ada pertanyaan, saran atau kritik hubungi kami di:</p>
+    <p>WA 0812344</p>
+    <p>Email : admin@gmail.com</p>
+</section>
+
+<footer>
+    <p>&copy; 2023 Restoran Enak .Hak Cipta di Lindungi</p>
+</footer>
+</body>
+</html>
